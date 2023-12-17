4a102794ed8a53ae3bb56749f9561774337478e2
EtienneGomez <egomezb1701@alumno.ipn.mx>
2023-12-17 02:34:02 -0600

    Update IFileSytemStorage.java

--- src/main/java/com/api/downupload/service/IFileSytemStorage.java
+++ src/main/java/com/api/downupload/service/IFileSytemStorage.java
@@ -5,7 +5,6 @@ import org.springframework.web.multipart.MultipartFile;
 import com.api.downupload.model.Documento;
 
 public interface IFileSytemStorage {
-	void init();
 	Documento saveFile(MultipartFile file);
 	Resource loadFile(Long idDocumento);
 }
