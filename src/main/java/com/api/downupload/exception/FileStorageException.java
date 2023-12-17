e5a5a9aea9f0d7965e8647be2ffdb1762f1e22f0
EtienneGomez <egomezb1701@alumno.ipn.mx>
2023-12-17 02:17:00 -0600

    Update FileStorageException.java

--- src/main/java/com/api/downupload/exception/FileStorageException.java
+++ src/main/java/com/api/downupload/exception/FileStorageException.java
@@ -1,21 +1,21 @@
 package com.api.downupload.exception;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class FileStorageException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	private String message;
 
 	public FileStorageException(String message) {
 		super(message);
+		this.message = message;
 	}
 
 	public FileStorageException(String message, Throwable cause) {
 		super(message, cause);
+		this.message = message;
 	}
 }
